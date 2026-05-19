599f56ca2c8c030e4562a197261af97e9ab878a9
enormous <enormous@RAJAN.local>
2026-05-19 12:08:59 +0530

    firebase

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,3 +1,6 @@
+import { auth } from './firebase'
+console.log('Firebase auth initialized:', auth)
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
