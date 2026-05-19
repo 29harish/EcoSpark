6ce77ea1c986e265d32bf0fcea9f600a167ec019
enormous <enormous@RAJAN.local>
2026-05-19 12:16:17 +0530

    Firebase auth and storage configured

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,5 @@
-import { auth } from './firebase'
-console.log('Firebase auth initialized:', auth)
-
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
