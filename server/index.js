4ca9b639dbe8c26913fb0dfee576f5545365bc24
enormous <enormous@RAJAN.local>
2026-05-19 12:11:33 +0530

    firebase

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
+require('dotenv').config()
+
+
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
-require('dotenv').config()
 
-console.log('MONGO_URI:', process.env.MONGO_URI) 
+
 
 const app = express()
 const PORT = process.env.PORT || 8000
